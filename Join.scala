d8116424622ae2b4ea9a9f577a98ddca168903a7
Parvatheesam <57515686+Parva-Github@users.noreply.github.com>
2020-05-28 13:31:40 -0400

    Update Join.scala

--- Join.scala
+++ Join.scala
@@ -6,7 +6,13 @@ trait Join[L,R,Q] {
 
 case class JoinOutput(left: Any, right: Option[Any])
 
+class StaticMapJoin[L,R] (val joinCond: (L) => String ) (val joinCond1: (R) => String ) extends Join[L,R, JoinOutput] {
 
+  override def join(a: List[L], b: List[R]): List[JoinOutput] = {
+    val l: Map[String, R] = b.map(b => joinCond1(b) -> b).toMap
+    a.filter(a => l.contains(joinCond(a))).map(a => JoinOutput(a, Some(l(joinCond(a)))))
+  }
+}
 class GenericNestedLoopJoin[L, R](val joinCond: (L, R) => Boolean) extends Join[L, R, JoinOutput] {
   override def join(a: List[L], b: List[R]): List[JoinOutput] = for {
     l <- a
