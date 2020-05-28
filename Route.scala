6be2a8e44f59b4854b531ba0064004b54ca99728
kishore09134 <63380692+kishore09134@users.noreply.github.com>
2020-05-28 13:29:55 -0400

    Add files via upload

---
+++ Route.scala
@@ -0,0 +1,30 @@
+package org.kishore.scala.mcit
+case class Route(
+                  route_id: Int,
+                  agency_id: String,
+                  route_short_name: String,
+                  route_long_name: String,
+                  route_type: String,
+                  route_url: String,
+                  route_color: String,
+                  route_text_color: String
+                )
+
+
+object Route {
+  def apply(csvLine: String): Route = {
+    val p = csvLine.split(",",-1)
+    new Route(p(0).toInt, p(1), p(2), p(3), p(4), p(5), p(6),p(7))
+  }
+  def toCsv(route: Route): String = {
+    route.route_id + "," +
+      route.agency_id + "," +
+      route.route_short_name + "," +
+      route.route_long_name + "," +
+      route.route_type + "," +
+      route.route_url + "," +
+    route.route_color + "," +
+      route.route_text_color
+  }
+}
+
