7dcbb4e88801aca585a2e9f369d5ca4350e59c85
kishore09134 <63380692+kishore09134@users.noreply.github.com>
2020-05-28 13:44:03 -0400

    Update Join.scala

--- Join.scala
+++ Join.scala
@@ -5,14 +5,17 @@ trait Join[L,R,Q] {
 }
 
 case class JoinOutput(left: Any, right: Option[Any])
-
-class StaticMapJoin[L,R] (val joinCond: (L) => String ) (val joinCond1: (R) => String ) extends Join[L,R, JoinOutput] {
+//StaticMapJoin is between left and right protion of mapping And also lookup on smalltable becausecommon element
+class StaticMapJoin[L,R] (val joinCon: (L) => String ) (val joinCon1: (R) => String ) extends Join[L,R, JoinOutput] {
 
   override def join(a: List[L], b: List[R]): List[JoinOutput] = {
-    val l: Map[String, R] = b.map(b => joinCond1(b) -> b).toMap
-    a.filter(a => l.contains(joinCond(a))).map(a => JoinOutput(a, Some(l(joinCond(a)))))
+    val l: Map[String, R] = b.map(b => joinCon1(b) -> b).toMap
+    a.filter(a => l.contains(joinCon(a))).map(a => JoinOutput(a, Some(l(joinCon(a)))))
   }
 }
+
+//This an nesetd loopjoin between two lists
+
 class GenericNestedLoopJoin[L, R](val joinCond: (L, R) => Boolean) extends Join[L, R, JoinOutput] {
   override def join(a: List[L], b: List[R]): List[JoinOutput] = for {
     l <- a
